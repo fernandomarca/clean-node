4c7e8e1bba387cac070b4f8589cb3f145b3a8eb5
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-06-28 20:21:28 -0300

    ensure return an user if user is found

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['**/src/**/*.js'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8'
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb'
 }
