69548ff2080f07ee812e7179155ddd8e63a6fca8
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-06-28 18:47:43 -0300

    ensure tokengenerator returns null  if jwt returns null

--- myArquiteture.js
+++ myArquiteture.js
@@ -1,48 +1,48 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 module.exports = () => {
-  const router = new SignUpRouter()
-  router.post('/signup', ExpressRouterAdapter.adapt(router))
-}
+  const router = new SignUpRouter();
+  router.post("/signup", ExpressRouterAdapter.adapt(router));
+};
 
 class ExpressRouterAdapter {
-  static adapt (router) {
+  static adapt(router) {
     return async (req, res) => {
       const httpRequest = {
-        body: req.body
-      }
-      const httpResponse = await router.route(httpRequest)
-      res.status(httpResponse.statusCode).json(httpResponse.body)
-    }
+        body: req.body,
+      };
+      const httpResponse = await router.route(httpRequest);
+      res.status(httpResponse.statusCode).json(httpResponse.body);
+    };
   }
 }
 
 // signup-route = //Presentation
 class SignUpRouter {
-  async route (httpRequest) {
-    const { email, password, repeatPassword } = httpRequest.body
-    const user = new SignUpUseCase().signUp(email, password, repeatPassword)
+  async route(httpRequest) {
+    const { email, password, repeatPassword } = httpRequest.body;
+    const user = new SignUpUseCase().signUp(email, password, repeatPassword);
     return {
       statusCode: 200,
-      body: user
-    }
+      body: user,
+    };
   }
 }
 
 // signup-usecase //regras de negocio = Domain
 class SignUpUseCase {
-  async signUp (email, password, repeatPassword) {
+  async signUp(email, password, repeatPassword) {
     if (password === repeatPassword) {
-      new AddAccountRepository().add(email, password)
+      new AddAccountRepository().add(email, password);
     }
   }
 }
 
 // add-account-repo // Infra
 class AddAccountRepository {
-  async add (email, password) {
-    const user = await AccountModel.create({ email, password })
-    return user
+  async add(email, password) {
+    const user = await AccountModel.create({ email, password });
+    return user;
   }
 }
